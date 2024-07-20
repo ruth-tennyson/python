8d8b453467202d6e53160c9229936b1d761e1b96
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-07-20 12:24:47 +0530

    20/10

---
+++ bst.py
@@ -0,0 +1,63 @@
+class box:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+class ll:
+    def __init__(self):
+        self.root=None
+    def ins(self,data):
+        nn=box(data)
+        self.insert(self.root,nn)
+    def insert(self,root,nn):
+        if self.root==None:
+            self.root=nn
+            return
+        elif nn.data<root.data:
+            if root.left!=None:
+                self.insert(root.left,nn)
+            else:
+                root.left=nn
+        else:
+            if root.right!=None:
+                self.insert(root.right,nn)
+            else:
+                root.right=nn
+    def inittrav(self):
+        self.tra(self.root)
+    def tra(self,root):
+        if root==None:
+            return
+        else:
+            print(root.data,":",sep="",end="")
+            if root.left!=None:
+                print("L:",root.left.data,",",sep="",end="")
+            if root.right!=None:
+                print("R:",root.right.data,",",sep="",end="")
+            print()
+            self.tra(root.left)
+            self.tra(root.right)
+    def search(self,key):
+        root=self.root
+        check=0
+        while True:
+            if root==None:
+                break
+            else:
+                if root.data==key:
+                    check=1
+                    print("True")
+                    break
+                elif key<root.data:
+                    root=root.left
+                elif key>root.data:
+                    root=root.right
+        if check==0:
+            print("False")
+l=ll()
+n=int(input())
+list=list(map(int,input().split()))
+for i in list:
+    l.ins(i)
+n=int(input())
+l.search(n)
