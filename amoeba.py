9916424dacb4e435c905e8971d3b28432879d46a
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-09-28 22:42:28 +0530

    28/09

--- amoeba.py
+++ amoeba.py
@@ -1,3 +1,31 @@
+# The Research team led by Bernadette Wolowitz at Cal-tech University has discovered a new Amoeba that grows in the order of a Fibonacci series every month. They are exhibiting this amoeba at a national conference. They want to know the size of the amoeba at a particular time instant. If a particular month’s index is given, write a program to display the amoeba’s size.
+
+# For Example, the size of the amoeba in months 1, 2, 3, 4, 5, 6,... will be 0, 1, 1, 2, 3, 5, 8.... respectively.
+
+# Input Format
+
+# The input is an integer that denotes the count of the month.
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# The output is an integer denoting the amoeba size.
+
+# Sample Input 0
+
+# 13
+# Sample Output 0
+
+# 144
+# Sample Input 1
+
+# 9
+# Sample Output 1
+
+# 21
 mon=int(input())
 fib0=0
 fib1=1
--- collatz.py
+++ collatz.py
@@ -1,3 +1,48 @@
+# Chander started working for Bing and he wanted him to write a program to generate Collatz Sequence. The rules for generating the Collatz sequence are: If n is even, n = n / 2. If n is odd, n = 3n + 1.
+
+# For example, if the starting number is 5 the sequence is: 5 -> 16 -> 8 -> 4 -> 2 -> 1 It has been proved for almost all integers, that the repeated application of the above rule will result in a sequence that ends in 1.
+
+
+
+# Input Format
+
+# The input is an integer 'n' which denotes the first term of the sequence.
+
+
+
+# Output Format
+
+# As output, print the numbers in the sequence and also print the number of times the rule has to be applied in order to reach 1.
+
+
+
+# Sample Input 0
+
+# 18
+
+# Sample Output 0
+# 18
+# 9
+# 28
+# 14
+# 7
+# 22
+# 11
+# 34
+# 17
+# 52
+# 26
+# 13
+# 40
+# 20
+# 10
+# 5
+# 16
+# 8
+# 4
+# 2
+# 1
+# 20
 n=int(input())
 count=0
 while n>1:
